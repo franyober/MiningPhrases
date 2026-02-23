087e3ccf2e4e2fe06041f13626ecf2f9bab22595
fran.garzon <fran.garzon@dev.pro>
2026-02-23 09:33:07 -0500

    corrigiendo modelo de TTS

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_KEY_API")
 ANKI_CONNECT_URL = os.getenv("ANKI_CONNECT_URL", "http://localhost:8765")
 
 GEMINI_MODEL = "gemini-2.5-flash"
-TTS_MODEL = "gemini-2.0-flash-exp" # Or gemini-2.0-flash-001
+TTS_MODEL = "gemini-2.5-flash-preview-tts"
 
 
 # Anki Settings
